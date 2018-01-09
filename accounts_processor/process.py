22770c369cbc590ff4f3e868c0458d3bd0581c6a
Alex <alex@growthintel.com>
2018-01-09 13:27:59 +0000

    Added subcategory processing

--- accounts_processor/process.py
+++ accounts_processor/process.py
@@ -1,5 +1,24 @@
-import pandas as pd
+"""Accounts processor
 
+Usage:
+    process.py <input> <output>
+"""
+from docopt import docopt
+
+import pandas as pd
+from pandas import np
+
+from scripts.accounts_processor import subcategories
 
 if __name__ == '__main__':
-    print(pd.__version__)
+    args = docopt(__doc__)
+    accounts_data = pd.read_csv(args['<input>'], skiprows=4, encoding='latin_1')
+
+    accounts_data['category'] = np.nan
+    accounts_data['Description'] = accounts_data['Description'].str.lower()
+
+    for subcategory in subcategories.all_subcategories:
+        accounts_data.loc[accounts_data['Description'].str.contains(subcategory.description_regex),
+                          'category'] = str(subcategory.category)
+
+    print(accounts_data[accounts_data.category.notnull()])
--- accounts_processor/subcategories.py
+++ accounts_processor/subcategories.py
@@ -1,7 +1,22 @@
-class SubCategory(object):
+class Category(object):
+    def __init__(self, name):
+        self.name = name
 
+    def __str__(self):
+        return self.name
+
+
+food_shopping = Category("Food Shopping")
+
+
+class SubCategory(object):
     def __init__(self, description_regex, category):
         self.description_regex = description_regex
         self.category = category
 
 
+waitrose = SubCategory(r'waitrose', food_shopping)
+
+all_subcategories = [
+    waitrose
+]
